605a6f8538b51b1c22219940568362acdd9de093
Lydia Young <lydsjoyy@gmail.com>
2023-05-26 16:11:36 +0000

    Tidy up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,22 @@ document.addEventListener("DOMContentLoaded", function() {
         })
 
     }
+    
+    document.getElementById("answer-box").addEventListener("keydown", function(event) { 
+     if (event.key === "Enter") {
+        checkAnswer();
+     }
+    })
 
     runGame("addition");
+
 })
 
 function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
