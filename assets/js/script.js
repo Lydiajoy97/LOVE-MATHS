70baade15bb952c9ce6dae48a4b718d04375a4d6
Lydia Young <lydsjoyy@gmail.com>
2023-05-26 13:41:13 +0000

    Set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer () {
+
+}
+
+function displayAdditionQuestion () {
+
+}
+
+function displaySubtractQuestion () {
+
+}
+
+function displayMultiplyQuestion () {
+    
+}
